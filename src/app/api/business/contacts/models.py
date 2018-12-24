05fe2a33e622dcb8f78504294135e485a5a24330
jlaplante333 <jonathan.laplante@gmail.com>
2018-12-24 12:28:46 -0800

    code changes

--- src/app/api/business/contacts/models.py
+++ src/app/api/business/contacts/models.py
@@ -31,49 +31,7 @@ https://en.wikipedia.org/wiki/ISO_3166-2
     'region': fields.region(readOnly=True, description='Region of contact address(e.g. State, Province, Territory, Region, District)'), //Please read: https://en.wikipedia.org/wiki/ISO_3166-2
     'street': fields.street(readOnly=True, description='Street of contact address'),
     'postal-code': fields.String(readOnly=True, description='Postal Code of contact address'),
-    'latitude': fields.latitude(readOnly=True, description='Latitude of contact address'),
-    'longitude': fields.longitude(readOnly=True, description='Longitude of contact address'),
     'alternate-address': fields.address(readOnly=True, description='Alternate address details of a contact, compound form'),
-    'alternate-latitude': fields.latitude(readOnly=True, description='Alternate latitude of contact address'),
-    'alternate-longitude': fields.longitude(readOnly=True, description='Alternate longitude of contact address'),
-
-"""
-
-phone number information
-
-"""
-
- 'phone-number': fields.Phone(readOnly=True, description='Phone Number of a contact, label should be business phone'),
- 'home-phone': fields.Phone(readOnly=True, description='Phone Number of a contact'),
- 'do-not-call': fields.Boolean(readOnly=True, description='Indicates that the contact does not want to receive calls.'),
- 
- 
-"""
-
-email information
-
-"""
-
-'email': fields.Email(readOnly=True, description='Email of contact address'),
-'Opted-out-of-email': fields.Boolean(readOnly=True, description='Indicates whether the contact doesn’t want to receive email'),
-
-"""
-
-fax information
-
-"""
-'fax': fields.fax(readOnly=True, description='Fax number of contact.'),
-'opted-out-of-fax': fields.Boolean(readOnly=True, description='Indicates whether the contact prohibits receiving faxes.'),
-
-
-"""
-
-activity information
-
-"""
-'last-activity-date': fields.date(readOnly=True, description='Last date where an event was logged for a contact.'),
-'last-viewed-date': fields.date(readOnly=True, description='Last date where a contact was viewed.'),
-
 
 
 })
