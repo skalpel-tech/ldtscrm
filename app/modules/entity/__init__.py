a231d4d1ab7b2532b249436fcc1960f52553b6a5
cedar-technologies <alexandre.jx@gmail.com>
2019-01-06 21:02:09 -0800

    udate docstring

--- app/modules/entity/__init__.py
+++ app/modules/entity/__init__.py
@@ -5,7 +5,7 @@ Entity module
 
 Entity reprents nodes in the graph model.
 Entity content is defined and stored as a json object.
-Entity allow you to manage any object model by it's type.
+Entity allow you to manage any object model by its type.
 
 """
 def init_app(app, **kwargs):#pylint: disable=W0613
